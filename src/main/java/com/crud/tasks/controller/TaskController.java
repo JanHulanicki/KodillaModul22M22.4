dc1bd957f1ee24d12239d8adb5021e2046057492
Jan Hulanicki <jan_hulanicki@poczta.onet.pl>
2020-01-17 19:58:27 +0100

    M19.2 Zadanie: Utwórz metodę do wyszukiwania pojedynczego zadania

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,18 +1,24 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 @RestController
 @RequestMapping("/v1/task")
 public class Controller {
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        return taskMapper.mapToTasskDtoList(service.getAllTasks());
     }
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto  getTask(Long taskId){
@@ -21,7 +27,7 @@ public class Controller {
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
     public void deleteTask(Long taskId){
     }
-    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
+    @RequestMapping(method = RequestMethod.GET, value = "updateTask")
     public TaskDto updateTask(TaskDto taskDto){
         return new TaskDto(1L,"Edited test title","Test content");
     }
