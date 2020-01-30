723b91535413f08f1293e01b59534f52a567c499
Jan Hulanicki <jan_hulanicki@poczta.onet.pl>
2020-01-30 18:54:20 +0100

    Zadanie: Skrypt do uruchamiania przeglądarki

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,9 +1,19 @@
 package com.crud.tasks;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
-public class TasksApplication {
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+
+public class TasksApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(TasksApplication.class);
 	}
 }
